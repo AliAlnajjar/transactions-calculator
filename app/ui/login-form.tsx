7c2bb075bf6377341db267e94b370fad61075f71
Ali Alnajjar <ali.alnajjar@Alis-MacBook-Pro-2.local>
2023-12-08 17:23:17 +0100

    arabic lables for log-in form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -17,16 +17,17 @@ export default function LoginForm() {
   return (
     <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+        <h1 className={`${lusitana.className} mb-3 text-2xl text-center`}>
+      
+      يرجى تسجيل الدخول أولاً
         </h1>
         <div className="w-full">
           <div>
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className="mb-3 mt-5 block text-sm font-medium text-gray-900 text-right text-right"
               htmlFor="email"
             >
-              Email
+              البريد الإلكتروني
             </label>
             <div className="relative">
               <input
@@ -34,7 +35,7 @@ export default function LoginForm() {
                 id="email"
                 type="email"
                 name="email"
-                placeholder="Enter your email address"
+                placeholder="ادخل البريد الإلكتروني"
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -42,10 +43,10 @@ export default function LoginForm() {
           </div>
           <div className="mt-4">
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              className="mb-3 mt-5 block text-sm font-medium text-gray-900 text-right"
               htmlFor="password"
             >
-              Password
+              كلمة المرور
             </label>
             <div className="relative">
               <input
@@ -53,7 +54,7 @@ export default function LoginForm() {
                 id="password"
                 type="password"
                 name="password"
-                placeholder="Enter password"
+                placeholder="ادخل كلمة المرور"
                 required
                 minLength={6}
               />
@@ -84,7 +85,7 @@ function LoginButton() {
 
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+      تسجيل الدخول  <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
 }
